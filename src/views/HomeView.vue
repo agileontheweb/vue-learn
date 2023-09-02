d53ce6adc5994b504fd7d98f8a0000ffc861ba1c
alessandro <agileontheweb@gmail.com>
2023-09-02 18:07:33 +0200

    Rename Welcome to ThisProject and refactor

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/ThisProject.vue'
+import ThisProject from '../components/ThisProject.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <ThisProject />
   </main>
 </template>
