9bed04c92ed57d5fe3c3dcb43830957c83efeabf
alessandro <agileontheweb@gmail.com>
2023-09-02 18:01:28 +0200

    Rename components and refactor. This is a Home page

---
+++ src/components/ThisProject.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <div class="border bg-gray-100">
+    <div class="max-w-screen-md mx-auto py-8">
+      
+        <h2 class="text-2xl mt-6">Documentation</h2>
+        <p>This is my first Vue Project. I developed it to test how it work and to show this small project
+          at the agency to find a job.
+        </p>
+      
+        <h2 class="text-2xl mt-6">Tools</h2>
+        <p>I installed Vue 3 via node and then added Tailwind as style processor.
+        More instructions are available in <code>README.md</code>.</p>
+    </div>
+  </div>
+</template>
