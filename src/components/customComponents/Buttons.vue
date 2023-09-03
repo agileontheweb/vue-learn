9388a837b0a4688a872c1f5521e5eaf18419d57f
alessandro <agileontheweb@gmail.com>
2023-09-03 10:26:04 +0200

    Fix Button. hide - button if less 0

--- src/components/customComponents/Buttons.vue
+++ src/components/customComponents/Buttons.vue
@@ -7,10 +7,10 @@
           +
         </button>
 
-        <button @click="state.count--" class="px-3 py-1 border border-gray-500 mx-2 rounded-md" :class="{ 'hidden': state.count === 0 }">
+        <button @click="state.count--" class="px-3 py-1 border border-gray-500 mx-2 rounded-md" :class="{ 'hidden': state.count < 0 }">
           -
         </button>
-          <p class="mt-3 bg-white text-center inline-block p-2">  {{ state.count < 0 ? 'Errore: A meno di ZERO non posso andare' : state.count }}</p>
+          <p class="mt-3 bg-white text-center inline-block p-2">  {{ state.count < 0 ? 'Errore: A meno di ZERO non puoi andare' : state.count }}</p>
         </div>
     </div>
 </template>
