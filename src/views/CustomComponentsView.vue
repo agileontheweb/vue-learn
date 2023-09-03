002004d70fd4a492edb93b3de7f1809128e985ad
alessandro <agileontheweb@gmail.com>
2023-09-03 10:26:50 +0200

    Add / remove components

--- src/views/CustomComponentsView.vue
+++ src/views/CustomComponentsView.vue
@@ -1,17 +1,20 @@
+<script setup>
+  import Buttons from '../components/customComponents/Buttons.vue';
+  
+  import Message from '../components/customComponents/Message.vue';
+  </script>
+
 <template>
   <div class="border bg-gray-100">
     <div class="max-w-screen-md mx-auto py-8">      
         <h2 class="text-2xl mt-6">Custom Components</h2>
         <p>In this section i created some components to use in the project.</p>
+
+        <Message />
         <Buttons />
-        <SlideImages />
         
     </div>
   </div>
-
   </template>
-  <script setup>
-  import Buttons from '../components/customComponents/Buttons.vue';
-  import SlideImages from '../components/customComponents/SlideImages.vue';
-  </script>
+  
   
