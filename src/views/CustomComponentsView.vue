cbdafe2de162a488a56840e13e2a6810ed81b485
alessandro <agileontheweb@gmail.com>
2023-09-04 10:51:43 +0200

    Refactor

--- src/views/CustomComponentsView.vue
+++ src/views/CustomComponentsView.vue
@@ -1,6 +1,5 @@
 <script setup>
   import Buttons from '../components/customComponents/Buttons.vue';
-  
   import Message from '../components/customComponents/Message.vue';
   import Input from '../components/customComponents/Input.vue';
   import Gmap from '../components/customComponents/Gmap.vue';
@@ -8,10 +7,16 @@
   </script>
 
 <template>
-  <div class="border bg-gray-100">
+  <div class="bg-gray-200">
     <div class="max-w-screen-md mx-auto py-8">      
-        <h2 class="text-2xl mt-6">Custom Components</h2>
+        <h1 class="text-3xl mt-6">Custom Components</h1>
         <p>In this section i created some components to use in the project.</p>
+    </div>
+    <div class="bg-gray-200">
+      <div class="max-w-screen-md mx-auto py-8">
+        <Form  />
+      </div>
+    </div>
 
     <div class="bg-white">
       <div class="max-w-screen-md mx-auto py-8">      
@@ -22,8 +27,12 @@
     <div class="bg-gray-200">
       <div class="max-w-screen-md mx-auto py-8">      
         <Message />
+      </div>
+    </div>
+    <div class="bg-white">
+      <div class="max-w-screen-md mx-auto py-8">
         <Buttons />
-        
+      </div>
     </div>
   </div>
   </template>
