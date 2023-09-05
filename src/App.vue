8c0c4ae44812dc33420c315e2075109a55a2e274
alessandro <agileontheweb@gmail.com>
2023-09-05 09:37:40 +0200

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,9 @@ import Navigation from './components/Navigation.vue'
   </div>
 
   <RouterView />
+  <footer class="bg-black">
+    <div class="max-w-screen-md mx-auto py-8 text-white">
+      <p>Alessandro Cuoghi - Example Vue Project</p>
+     </div>
+  </footer>
 </template>
