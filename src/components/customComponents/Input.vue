5fa477675403287dd9e8143bd577bdecf4031920
alessandro <agileontheweb@gmail.com>
2023-09-05 09:41:00 +0200

    Refactor input check Air quality
    Pass data to chart
    Add loading

--- src/components/customComponents/Input.vue
+++ src/components/customComponents/Input.vue
@@ -1,9 +1,101 @@
+<script setup>
+import BarChart from './BarChart.vue';
+import Loading from './Loading.vue';
+import {ref} from 'vue'
+import axios from 'axios';
+import { computed, onMounted } from 'vue';
+let searchTerm = ref('');
+let listCity = ref('');
+
+const searchQuery = ref('');
+const cities = ref([]);
+let searchTimeout;
+let showWeather = false;
+
+const isLoading = ref(false);
+const message = ref('');
+
+const filteredCities = computed(() => {
+  return cities.value.filter((city) =>
+    city.name.toLowerCase().startsWith(searchQuery.value.toLowerCase())
+  );
+});
+
+const searchCities = async () => {
+  try {
+    message.value = 'Loading cities X';  
+    isLoading.value = true;
+    const response = await fetch(
+      "https://raw.githubusercontent.com/lmfmaier/cities-json/master/cities500.json"
+    );
+    if (response.ok) {
+      cities.value = await response.json();
+      isLoading.value = false;
+    }
+  } catch (error) {
+    console.error("Errore durante il recupero delle città: ", error);
+  }
+};
+
+const onInputChange = () => {
+  clearTimeout(searchTimeout);
+  searchTimeout = setTimeout(() => {
+    searchCities();
+  }, 500);
+};
+
+const getPosition = (getPosition) => {
+    console.log("click:" + getPosition);
+    searchQuery.value = '';
+    showWeather = true;
+    getAirQulity(getPosition);
+    message.value = 'Loading Data City';  
+    isLoading.value = true;
+}
+let city = ref('');
+let CO = ref('');
+let NO2 = ref('');
+let O3 = ref('');
+let PM10 = ref('');
+// let SO2 = ref('');
+ const chartDataFromAPI = ref(null);
+
+const getAirQulity = (getPosition) => {
+    console.log("ususus");
+    city = getPosition;
+    axios.get('https://api.api-ninjas.com/v1/airquality', {
+    params: {
+      city: city,
+    },
+    headers: {
+        'X-Api-Key': 'B3c3dxfR39RYSoRrudMB3w==TAV6QtCyg9hU9gyi',
+    },
+  })
+  .then((response) => {
+    console.log(response.data);
+    console.log("CO: " + response.data.CO.concentration);
+    CO.value = response.data.CO.concentration;
+    console.log("NO2 : " + response.data.NO2.concentration);
+    NO2.value = response.data.NO2.concentration;
+    console.log("O3 : " + response.data.O3.concentration);
+    O3.value = response.data.O3.concentration;
+    console.log("PM10 : " + response.data.PM10.concentration);
+    PM10.value = response.data.PM10.concentration;
+    const chartData = [response.data.CO.concentration, response.data.NO2.concentration, response.data.O3.concentration, response.data.PM10.concentration];
+    chartDataFromAPI.value = chartData;
+    message.value = '';  
+    isLoading.value = false;
+  })
+  .catch((error) => {
+    console.error('Error:', error.response.data);
+  });
+};
+</script>
+
 <template>
-    <div class="mb-6">
-        <h2 class="text-2xl mt-6">AutoComplete + API: Air Quality</h2>
-        <p>Find city and get air quality</p>
-    </div>
+    <Loading :message="message" :isLoading="isLoading" />
     <div class="my-6">
+
         <input
         class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         v-model="searchQuery"
@@ -13,8 +105,12 @@
             <li class="p-2 cursor-pointer hover:text-white hover:bg-blue-500"
             @click="getPosition(city.name)" v-for="city in filteredCities" :key="city.geonameid">{{ city.name }}</li>
         </ul>
+
         <div v-if="showWeather" class="bg-white p-3 mt-3">
-            Quality Air in the city: <strong>{{ city }} </strong>
+            Quality Air in the city: <strong>{{ city }} </strong>            
+            
+            <BarChart :chartData="chartDataFromAPI" />
+
             <div class="grid  grid-cols-4 gap-3 text-center py-3">
                 <div class="border p-3 text-left bg-gray-100">
                     <h3 class="text-2xl my-2"> CO: {{ CO }}</h3>
@@ -44,89 +140,10 @@
                     </small>
                 </p>
                 </div>
-        </div>
+            </div>
         </div>
     </div>
+    
 
 </template>
 
-<script setup>
-import {ref} from 'vue'
-import axios from 'axios';
-import { computed, onMounted } from 'vue';
-let searchTerm = ref('');
-let listCity = ref('');
-
-const searchQuery = ref('');
-const cities = ref([]);
-let searchTimeout;
-let showWeather = false;
-
-const filteredCities = computed(() => {
-  return cities.value.filter((city) =>
-    city.name.toLowerCase().startsWith(searchQuery.value.toLowerCase())
-  );
-});
-
-const searchCities = async () => {
-  try {
-    const response = await fetch(
-      "https://raw.githubusercontent.com/lmfmaier/cities-json/master/cities500.json"
-    );
-    if (response.ok) {
-      cities.value = await response.json();
-    }
-  } catch (error) {
-    console.error("Errore durante il recupero delle città: ", error);
-  }
-};
-
-const onInputChange = () => {
-  clearTimeout(searchTimeout); // Cancella il timer precedente
-  searchTimeout = setTimeout(() => {
-    searchCities(); // Esegui la ricerca dopo un ritardo di, ad esempio, 500 ms
-  }, 500);
-};
-
-const getPosition = (getPosition) => {
-    console.log("click:" + getPosition);
-    searchQuery.value = '';
-    showWeather = true;
-    getAirQulity(getPosition);
-}
-let city = ref('');
-let CO = ref('');
-let NO2 = ref('');
-let O3 = ref('');
-let PM10 = ref('');
-// let SO2 = ref('');
-
-const getAirQulity = (getPosition) => {
-    city = getPosition;
-    axios.get('https://api.api-ninjas.com/v1/airquality', {
-    params: {
-      city: city,
-    },
-    headers: {
-        'X-Api-Key': 'B3c3dxfR39RYSoRrudMB3w==TAV6QtCyg9hU9gyi',
-    },
-  })
-  .then((response) => {
-    console.log(response.data);
-    console.log("CO: " + response.data.CO.concentration);
-    CO.value = response.data.CO.concentration;
-    console.log("NO2 : " + response.data.NO2.concentration);
-    NO2.value = response.data.NO2.concentration;
-    console.log("O3 : " + response.data.O3.concentration);
-    O3.value = response.data.O3.concentration;
-    console.log("PM10 : " + response.data.PM10.concentration);
-    PM10.value = response.data.PM10.concentration;
-    
-  })
-  .catch((error) => {
-    console.error('Error:', error.response.data);
-  });
-}
-
-
-</script>
