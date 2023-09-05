27cc31ce69724e4ef714956e8a1e529f7faa8650
alessandro <agileontheweb@gmail.com>
2023-09-05 09:41:22 +0200

    Loading Component

---
+++ src/components/customComponents/Loading.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-if="isLoading" class="loading">
+    <span class="bg-yellow-100 text-yellow-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">
+      {{ message }}
+    </span>
+  </div>
+
+</template>
+
+<script setup>
+    import { ref, defineProps } from 'vue';
+    const { message, isLoading } = defineProps(['message', 'isLoading']);
+
+
+</script>
