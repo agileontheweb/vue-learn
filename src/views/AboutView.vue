2280135daf95c6f8cd7c7b0a6724da9966bd8514
alessandro <agileontheweb@gmail.com>
2023-09-11 16:37:23 +0200

    Add Apsect ratio Tailwind video Youtube

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,6 +6,20 @@
           I like code and the field of Front End developer, i used in the past Angualar and Ruby on Rails,
           i get expierence also in React.
         </p>
+        <div class="relative ">
+          <iframe class="w-full aspect-video rounded-lg shadow-lg h-80"
+          src="https://www.youtube.com/embed/v1Nh3bj5MZI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" data-ruffle-polyfilled=""></iframe>
+        </div>
+
+        <div class="relative mt-6">
+          <iframe class="w-full aspect-video rounded-lg shadow-lg h-80"
+          src="https://www.youtube.com/embed/LO0v_g8C_2I" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" data-ruffle-polyfilled=""></iframe>
+        </div>
+
+        <div class="relative mt-6">
+          <iframe class="w-full aspect-video rounded-lg shadow-lg h-80"
+          src="https://www.youtube.com/embed/xNfo34HDNjM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" data-ruffle-polyfilled=""></iframe>
+        </div>
     </div>
   </div>
 </template>
