80d461b81ad3f2d7ab27c82dc12d9fac53636c8f
alessandro <agileontheweb@gmail.com>
2023-09-11 16:38:48 +0200

    Add vue-18n + Add File to languages

--- src/main.js
+++ src/main.js
@@ -3,8 +3,25 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createI18n } from 'vue-i18n';
+import messagesEn from './locales/en.json';
+import messagesIt from './locales/it.json';
+import messagesEs from './locales/es.json';
 
-const app = createApp(App)
-app.use(router)
-
-app.mount('#app')
+const i18n = createI18n({
+    legacy: false,
+    locale: 'it',
+    fallbackLocale: 'it',
+    messages: {
+      en: messagesEn,
+      it: messagesIt,
+      es: messagesEs,
+    },
+  });
+    
+  
+  const app = createApp(App);
+  app.use(router);
+  app.use(i18n);
+  app.mount('#app');
+  
