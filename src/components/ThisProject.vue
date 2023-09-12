dfdfd6f8de2762fdb4ac48b1ee3041378b4d0328
alessandro <agileontheweb@gmail.com>
2023-09-12 16:15:00 +0200

    Refactor IT.json

--- src/components/ThisProject.vue
+++ src/components/ThisProject.vue
@@ -1,5 +1,5 @@
 <script setup>
-  const totalHours = "55 hours";
+  const totalHours = "60";
   import { RouterLink } from 'vue-router';
   import { ref, defineProps, onMounted } from 'vue';
   import { useI18n } from 'vue-i18n';
@@ -13,7 +13,7 @@
   const currentDate = computed(() => {
     return new Date().toLocaleDateString();
   });
-
+  
   onMounted(() => {
     document.title = pageTitle.value;
     const metaDescription = document.querySelector('meta[name="description"]');
@@ -21,13 +21,9 @@
       metaDescription.setAttribute('content', pageDescription.value);
     }
   });
-
-
 </script>
 
-<template>
-      
-
+<template>      
   <div class="border bg-gray-100">
     <div class="container">
       <h1>
@@ -36,113 +32,103 @@
          Vue 3.
       </h1>
       <p>{{ t('thisProject.startedOn') }} </p>      
-        <p v-html="t('thisProject.today', { currentDate: currentDate, totalHours: totalHours })"></p>
+      <p v-html="t('thisProject.today', { currentDate: currentDate, totalHours: totalHours })"></p>
+      
+      <p>{{ t('thisProject.p_1') }}</p>
 
-          <p>To learn Vue3 i used:
-            <ul>
-              <li>
-                <a href="https://vuejs.org/guide/" target="_blank" rel="noopener">Vuejs</a>
-              </li>
-              <li>
-                <a href="https://www.youtube.com/watch?v=YrxBCBibVo0&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1" target="_blank" rel="noopener">Youtube</a>
-              </li>
-              <li>
-                <a href="http://stackoverflow.com/" target="_blank" rel="noopener">Stackoverflow</a>
-              </li>
-              <li>
-                <a href="https://chat.openai.com//" target="_blank" rel="noopener">ChatGPT </a>
-              </li>
-            </ul>
-          </p>
-          
-          <p>
-            I started to read the documentation to get a global idea as it work.
-            After i watch some youtube tutorial to have an global idea on how it work with some example.
-            And the end i created a 
-            <a href="https://github.com/agileontheweb/vue-learn/" target="_blank" rel="noopener">repository on github</a>
-
-             and develop my personal project.
-          </p>
-          
-          <p>
-            During the project i used ChatGPT to check sintax error or help me to understand some question i had.
-          </p>
-        
-        <h2>Tools</h2>
-        
-        <p>
-          To create this project i used this tool.
-          
-        </p>
-        <p>
-          <ul>
-            <li>Github</li>
-            <li>Node</li>
-            <li>Tailwind</li>          
-            <li>Firebase</li>
-          </ul>
-        </p>
-        <h2>Project Goal</h2>
-        <p>In this project i want to learn how to:</p>
-        <p>
-          <ul>
-            <li>
-              Install Vue.
-            </li>
-            <li>
-              Add TailWind.
-            </li>
-            <li>
-              Create Navigation Section (Router)
-            </li>
-            <li>
-              API Call.
-            </li>
-            <li>
-              Corousel
-            </li>
-            <li>
-              Form
-            </li>
-            <li>
-              Autocomplete
-            </li>
-            <li>
-              Google Maps
-            </li>
-            <li>
-              Loading
-            </li>
-            <li>
-              GraphChart
-            </li>
-            <li>
-              FontAwesome
-            </li>
-            <li>
-              Buttons: Increment Decrement
-            </li>
-            <li>
-              Cart: Add product and show into section
-            </li>
-            <li>
-              Crud: FireBase
-            </li>
-            <li>
-              vue-i18n
-            </li>
-            <li>
-              Deploy
-            </li>
-            
+      <ul>
+          <li>
+            <a href="https://vuejs.org/guide/" target="_blank" rel="noopener">Vuejs</a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/watch?v=YrxBCBibVo0&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1" target="_blank" rel="noopener">Youtube</a>
+          </li>
+          <li>
+            <a href="http://stackoverflow.com/" target="_blank" rel="noopener">Stackoverflow</a>
+          </li>
+          <li>
+            <a href="https://chat.openai.com//" target="_blank" rel="noopener">ChatGPT </a>
+          </li>
         </ul>
         
-        </p>
-        <p>
-          I created a section
-          <RouterLink :to="{ name: 'customComponents', params: { section: 'topPage' } }">components page</RouterLink>.
-          Check my custom components.
-        </p>
+        <p v-safe-html="t('thisProject.p_2')"></p>
+        <p>{{ t('thisProject.p_3') }}</p>
+        <h2>{{ t('thisProject.h2_tools') }}</h2>
+        <p>{{ t('thisProject.p_4') }}</p>
+        
+        <ul>
+          <li>Github</li>
+          <li>Node</li>
+          <li>Tailwind</li>          
+          <li>Firebase</li>
+        </ul>
+
+        <h2>{{ t('thisProject.h2_project_goal') }}</h2>
+        <p>{{ t('thisProject.p_5') }}</p>
+        
+        <ul>
+          <li>
+            {{ t('thisProject.list.li_0') }}
+          </li>
+          <li>
+            {{ t('thisProject.list.li_1') }}
+          </li>
+          <li>
+            {{ t('thisProject.list.li_2') }}
+          </li>
+          <li>
+            {{ t('thisProject.list.li_3') }}
+          </li>
+          <li>
+            {{ t('thisProject.list.li_4') }}
+          </li>
+          <li>
+            {{ t('thisProject.list.li_5') }}
+          </li>
+          <li>
+            {{ t('thisProject.list.li_6') }}
+          </li>
+          <li class="line-through	">
+            {{ t('thisProject.list.li_7') }}
+          </li>
+          <li>
+            {{ t('thisProject.list.li_8') }}
+          </li>
+          <li>
+            {{ t('thisProject.list.li_9') }}
+          </li>
+          <li>
+            {{ t('thisProject.list.li_10') }}
+          </li>
+          <li>
+            {{ t('thisProject.list.li_11') }}
+          </li>
+          <li>
+            {{ t('thisProject.list.li_12') }}
+          </li>
+          <li>
+            {{ t('thisProject.list.li_12a') }}
+          </li>
+          <li>
+            {{ t('thisProject.list.li_13') }}
+          </li>
+          <li>
+            {{ t('thisProject.list.li_14') }}
+          </li>
+          <li>
+            {{ t('thisProject.list.li_15') }}
+          </li>
+          <li>
+            {{ t('thisProject.list.li_16') }}
+          </li>
+      </ul>  
+      <p>
+        {{ t('thisProject.p_6') }}
+        <RouterLink :to="{ name: 'customComponents', params: { section: 'topPage' } }">
+          {{ t('thisProject.router_link') }}
+        </RouterLink>.
+        {{ t('thisProject.p_7') }}
+      </p>
     </div>
   </div>
 </template>
--- src/views/CustomComponentsView.vue
+++ src/views/CustomComponentsView.vue
@@ -103,7 +103,7 @@ const updateTotalItemCart = (updatedQuantity) => {
             <a>{{ t('customComponent.component_slide_image') }}</a>
           </li>
           <li @click="scrollToSection('inputSection')">
-            <a>{{ t('customComponent.component_autocomplete') }}</a>
+            <a>{{ t('customComponent.component_autocomplete.title') }}</a>
           </li>
           <li @click="scrollToSection('quoteSection')">
             <a>{{ t('customComponent.component_quote_api') }}</a>
@@ -123,36 +123,18 @@ const updateTotalItemCart = (updatedQuantity) => {
           <div class="mb-6">
             <h2>{{ t('customComponent.component_slide_image') }}</h2>
             <p>
-              In this components i can pass some params like title, description
-              and array images.
+              {{ t('customComponent.slideImage.p_1') }}
             </p>
             <p>
               <code>
                 &lt;Card productName="Product 1" productDescription="Description 1 " :images="imageSrcArrayOne"/&gt;
               </code>
-              
             </p>
             <p>
-              Inside Card i added two components.
-              </p>
-              <p>
-              <code>
-                &lt;SquareItem :images="images"/&gt;
-                <br>
-                &lt;Form /&gt;
-              </code>
+              {{ t('customComponent.slideImage.p_2') }}
             </p>
             <p>
-              Inside SquareItem is it Carousel component:
-            </p>
-            <p>
-              <code>
-                &lt;Carousel :images="images"/&gt;
-              </code>
-            </p>        
-            <p>
-              The Form instead, after complete and click submit show a message
-              with the name input .
+              {{ t('customComponent.slideImage.p_3') }}
             </p>
           </div>
           <Card productName="Product 1" productDescription="Description 1 " :images="imageSrcArrayOne"/>
@@ -163,25 +145,13 @@ const updateTotalItemCart = (updatedQuantity) => {
     <div class="bg-white">
       <section ref="inputSection" class="container">
         <div class="max-w-screen-md mx-auto py-8">      
-          <h2>{{ t('customComponent.component_autocomplete') }}</h2>
+          <h2>{{ t('customComponent.component_autocomplete.title') }}</h2>
           <p>
-            In this components show the air quality  of the city via API.
-            Entering the name of the city does a search for cities using the json file 
+            {{ t('customComponent.component_autocomplete.p_1') }}
             <a href="https://raw.githubusercontent.com/lmfmaier/cities-json/master/cities500.json">cities500.json</a>
           </p>
-          <p>
-            After load the city and click one, call the API
-            <a href="https://api.api-ninjas.com/v1/airquality">airquality</a>
-            and get data.
-          </p>
-          <p>
-            I showed also a Chart of data.
-          </p>
-          <p>
-            Disabled GMAP: <i>You must enable Billing on the Google Cloud Project
-              at <a href="https://console.cloud.google.com/project/_/billing/enable">billing/enable</a>
-          </i>
-          </p>
+          <p v-safe-html="t('customComponent.component_autocomplete.p_2')"></p>
+          <p v-safe-html="t('customComponent.component_autocomplete.p_3')"></p>
           <Input />   
           <!-- <Gmap  /> -->
         </div>
@@ -191,8 +161,9 @@ const updateTotalItemCart = (updatedQuantity) => {
     <div class="bg-gray-200">
       <section ref="quoteSection" class="container">
         <div class="max-w-screen-md mx-auto py-8">      
-          <h2>Quote API</h2>
-          <p>Show random Quote via Api.  from https://api.api-ninjas.com/v1/quotes . Use Axios.</p>
+          <h2>{{ t('customComponent.component_quote_api.title') }}</h2>
+          <p v-safe-html="t('customComponent.component_quote_api.p_1')"></p>
+          
           <p>Click button to load a new random quote</p>
           <Message />
         </div>
