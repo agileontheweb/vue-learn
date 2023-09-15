1c72be3741965d20519d620275eb3f69e78cc516
alessandro <agileontheweb@gmail.com>
2023-09-15 18:24:19 +0200

    [WIP] Stripe Checkout
    Disable import StripeCheckout : Broke Route Navigation

--- src/components/customComponents/StripeItem.vue
+++ src/components/customComponents/StripeItem.vue
@@ -21,48 +21,54 @@
   </template>
   
 <script setup>
-import { ref, onMounted } from 'vue';
-import { StripeCheckout } from '@vue-stripe/vue-stripe';
-import { STRIPE_SECRET_KEY } from '../../apikey';
+// import { StripeCheckout } from '@vue-stripe/vue-stripe'; //rompe route nella navigazione
+  import { ref, onMounted } from 'vue';
+  import { STRIPE_PUBLISHABLE_KEY } from '../../apikey';
+  import { STRIPE_SECRET_KEY } from '../../apikey';
 
-const secretKey = STRIPE_SECRET_KEY;
-const selectedItem = ref(null);
-const loading = ref(false);
+  const secretKey = STRIPE_SECRET_KEY;
+  const selectedItem = ref(null);
+  const publishableKey = STRIPE_PUBLISHABLE_KEY;
+  const loading = ref(false);
 const productPrice = ref(0);
 const { idPrice } = defineProps(['idPrice']);
 
-const formatPrice = (price) => {
+  const formatPrice = (price) => {
     if (!price) return '';
     const formattedPrice = (price / 100).toFixed(2);
     return `${formattedPrice} €`;
-};
+  };
   
-const getItemAndPrice = async (priceId) => {
+  const getItemAndPrice = async (priceId) => {
     try {
         loading.value = true;
-        const productResponse = await fetch('https://api.stripe.com/v1/products', {
-            headers: {
-            Authorization: `Bearer ${secretKey}`,
-            },
-        });
+      const productResponse = await fetch('https://api.stripe.com/v1/products', {
+        headers: {
+          Authorization: `Bearer ${secretKey}`,
+        },
+      });
   
-        if (productResponse.ok) {
-            const productData = await productResponse.json();
-            const product = productData.data.find((item) => item.default_price === priceId);
-            selectedItem.value = {
-                name: product.name,
-                description: product.description,
-                images: product.images,
-                price: await getPrice(priceId),
-            };
-        } else {
-            console.error('Errore nel caricamento dei prodotti da Stripe');
-        }
-        } catch (error) {
-            console.error('Errore nel caricamento dei prodotti:', error);
-        } finally {
-            loading.value = false;
-        }    
+      if (productResponse.ok) {
+        const productData = await productResponse.json();
+        console.log(productData)
+        const product = productData.data.find((item) => item.default_price === priceId);
+
+        selectedItem.value = {
+          name: product.name,
+          description: product.description,
+          images: product.images,
+          price: await getPrice(priceId),
+        };
+      } else {
+        console.error('Errore nel caricamento dei prodotti da Stripe');
+      }
+    } catch (error) {
+      console.error('Errore nel caricamento dei prodotti:', error);
+    } finally {
+        loading.value = false;
+  }
+
+    
   };
   
   const getPrice = async (priceId) => {
